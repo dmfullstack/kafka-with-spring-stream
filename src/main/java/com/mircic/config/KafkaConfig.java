9a8cf6950905b12a6750b746cbf27e73f9210ba7
Ivan Mircic <ivan.mircic@gmail.com>
2016-10-16 00:48:25 +0300

    - Init commit

---
+++ src/main/java/com/mircic/config/KafkaConfig.java
@@ -0,0 +1,23 @@
+package com.mircic.config;
+
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.core.MessageSource;
+import org.springframework.messaging.support.GenericMessage;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Configuration
+@EnableBinding(Source.class)
+public class KafkaConfig {
+
+    @Bean
+    @InboundChannelAdapter(value = Source.OUTPUT)
+    public MessageSource<String> timerMessageSource() {
+        return () -> new GenericMessage<>(new SimpleDateFormat().format(new Date()));
+    }
+}
